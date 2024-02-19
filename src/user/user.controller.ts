a4cad3dead0c0e8409fe3ecfb4d23e31c044e670
Tim Slipko <timophiys.study@gmail.com>
2024-02-19 23:16:10 +0100

    created project structure, implemented auth and guards

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('me')
+  getMe(@GetUser() user) {
+    return this.userService.getMe(user['userId']);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe(userId: number) {
+    return await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, email: true, firstName: true, lastName: true },
+    });
+  }
+}
